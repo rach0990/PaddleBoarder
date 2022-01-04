bd156cfe87a00d6d53ec4650ae99f9c22d59b29e
Rachael <83975479+rach0990@users.noreply.github.com>
2022-01-04 19:04:15 +0000

    Api response codes to account for more weather types

--- PaddleBoarder/Models/DescriptionData.cs
+++ PaddleBoarder/Models/DescriptionData.cs
@@ -10,7 +10,7 @@ namespace PaddleBoarder.Models
     {
         public readonly int[] IdRain = { 500, 501, 502, 503, 504, 511, 520, 521, 522, 531 };
         public readonly int[] IdSun = { 800, 801};
-        public readonly int[] IdCloud = { 801, 802, 803, 804 };
+        public readonly int[] IdCloud = { 801, 802, 803, 804, 701,741, 721 }; //701, mist. 721, haze. 741, fog
         public readonly int[] IdSnow = { 600, 601, 602, 611 ,612, 613, 615, 616, 620, 621, 622 };
         public readonly int[] IdThunder = { 200, 201, 202, 210, 211, 212, 221, 230, 231, 232 };
     }
